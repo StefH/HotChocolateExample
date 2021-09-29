e308962105e561862b247cb51d19894b5f164911
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-09-29 06:53:43 +0000

    Use HotChocolate 12.0.0

--- StarWars.ExtraGraphQL/MyDiagnosticEventListener.cs
+++ StarWars.ExtraGraphQL/MyDiagnosticEventListener.cs
@@ -1,23 +1,32 @@
-﻿using System;
+using System;
 using HotChocolate.Execution;
 using HotChocolate.Execution.Instrumentation;
 using Microsoft.Extensions.Logging;
 
 namespace StarWars.ExtraGraphQL
 {
-    public class MyDiagnosticEventListener : DiagnosticEventListener
+    public class MyDiagnosticEventListener : ExecutionDiagnosticEventListener
     {
-        private readonly ILogger _logger;
+        private readonly ILogger<MyDiagnosticEventListener> _logger;
 
         public MyDiagnosticEventListener(ILogger<MyDiagnosticEventListener> logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        public override IActivityScope ExecuteRequest(IRequestContext context)
+        public override IDisposable ExecuteRequest(IRequestContext context)
         {
             _logger.LogInformation("ExecuteRequest ---");
-            return base.ExecuteRequest(context);
+
+            try
+            {
+                return base.ExecuteRequest(context);
+            }
+            catch (Exception e)
+            {
+                string s = e.Message;
+                throw;
+            }
         }
     }
 }
