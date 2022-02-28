433d5b66913cf18b7fbe745aadbe996a6917ded4
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-02-28 12:24:28 +0100

    refactor

--- StarWars/Characters/UseConvertUnitAttribute.cs
+++ StarWars/Characters/UseConvertUnitAttribute.cs
@@ -5,9 +5,7 @@ using HotChocolate.Types.Descriptors;
 
 namespace StarWars.Characters
 {
-    [AttributeUsage(
-        AttributeTargets.Property | AttributeTargets.Method,
-        AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method, AllowMultiple = false)]
     public sealed class UseConvertUnitAttribute : DescriptorAttribute
     {
         protected override void TryConfigure(
@@ -15,24 +13,26 @@ namespace StarWars.Characters
             IDescriptor descriptor,
             ICustomAttributeProvider element)
         {
-            if (descriptor is IObjectFieldDescriptor objectField)
+            switch (descriptor)
             {
-                objectField
-                    .Argument("unit", a => a.Type<EnumType<Unit>>().DefaultValue(Unit.Meters))
-                    .Use(next => async context =>
-                    {
-                        await next(context).ConfigureAwait(false);
-
-                        if (context.Result is double length)
+                case IObjectFieldDescriptor objectField:
+                    objectField
+                        .Argument("unit", a => a.Type<EnumType<Unit>>().DefaultValue(Unit.Meters))
+                        .Use(next => async ctx =>
                         {
-                            context.Result = ConvertToUnit(length, context.ArgumentValue<Unit>("unit"));
-                        }
-                    });
-            }
-            else if (descriptor is IInterfaceFieldDescriptor interfaceField)
-            {
-                interfaceField
-                    .Argument("unit", a => a.Type<EnumType<Unit>>().DefaultValue(Unit.Meters));
+                            await next(ctx).ConfigureAwait(false);
+
+                            if (ctx.Result is double length)
+                            {
+                                ctx.Result = ConvertToUnit(length, ctx.ArgumentValue<Unit>("unit"));
+                            }
+                        });
+                    break;
+
+                case IInterfaceFieldDescriptor interfaceField:
+                    interfaceField
+                        .Argument("unit", a => a.Type<EnumType<Unit>>().DefaultValue(Unit.Meters));
+                    break;
             }
         }
 
@@ -42,6 +42,7 @@ namespace StarWars.Characters
             {
                 return length * 3.28084d;
             }
+
             return length;
         }
     }
